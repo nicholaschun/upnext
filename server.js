a8f288270db64e8a9797ab0079731fa23846e3ba
nicholaschun <nicholaschunrayne@gmail.com>
2019-05-24 15:37:21 -0700

    added support for frontend

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 8080
 const hostname = '127.0.0.1'
-const users = require('./src/routes/users')
-const events = require('./src/routes/events')
-const url = require('./src/utils/urls')
-require('dotenv/config')
-
+const users = require('./src/backend/routes/users')
+const events = require('./src/backend/routes/events')
+const url = require('./src/backend/utils/urls')
+const web = require('./src/frontend/routes/index')
+app.set('views', './src/frontend/views')
+app.set('view engine', 'pug')
+app.use(express.static('./src/frontend/public'))
 
 app.use(`${url.users}`, users)
 app.use(`${url.events}`, events)
 
-app.get('/', (req, res) => {
-    res.write('this is an api for upnext')
-    res.end()
-})
+app.use('/', web)
 
 app.listen(port, () => {
     console.log(`Running on http://${hostname}:${port}`)
