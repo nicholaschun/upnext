40768a7b5125e7b3f966017e32d7093665e9ccdf
nicholaschun <nicholaschunrayne@gmail.com>
2019-05-29 21:12:23 -0700

    added support for database migrations with sequelize

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 const port = process.env.port || 8080
 const hostname = '127.0.0.1'
@@ -9,9 +10,15 @@ const web = require('./src/frontend/routes/index')
 app.set('views', './src/frontend/views')
 app.set('view engine', 'pug')
 app.use(express.static('./src/frontend/public'))
-
 app.use(`${url.users}`, users)
 app.use(`${url.events}`, events)
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'upnext'
+})
 
 app.use('/', web)
 
