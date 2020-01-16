458c513b130d607483d0ffe27bb75129e7a5ad85
nicholaschun <nicholaschunrayne@gmail.com>
2020-01-16 04:07:44 +0000

    added event skeleton to page

--- src/frontend/public/js/components/dashboard/dashboard.vue
+++ src/frontend/public/js/components/dashboard/dashboard.vue
@@ -1,26 +1,28 @@
 <template>
-    <div>
-         <div class="col-md-3">
-             <side-nav title="Dashboard" />    
-        </div>
-        <div class="col-md-9">
-            <lineup-nav />
-        </div>
+  <div>
+    <div class="col-md-3">
+      <side-nav title="Dashboard" />
     </div>
+    <div class="col-md-9">
+      <!-- <lineup-nav /> -->
+
+      <div class="col-md-12">
+        <events />
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 const lineupNav = require('./subComponents/lineupNav.vue')
 const sideNav = require('./subComponents/sideNav.vue')
-module.exports =  {
+const events = require('./subComponents/events.vue')
 
-components: {
-    'lineup-nav' : lineupNav,
-    'side-nav' : sideNav
-}
+module.exports = {
+  components: {
+    'lineup-nav': lineupNav,
+    'side-nav': sideNav,
+    events: events
+  }
 }
 </script>
-<style scoped>
-
-</style>
-
-
+<style scoped></style>
---
+++ src/frontend/public/js/components/dashboard/subComponents/eventCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="col-md-6" v-for="event in events" :key="event.id">
+      <div class="col-md-12 single-event">
+        <div class="event-image-container"></div>
+        <div class="event-caption-container"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+module.exports = {
+  props: {
+    events: Array
+  }
+}
+</script>
+<style scoped>
+.single-event {
+  background-color: white;
+  height: 200px;
+  margin: 15px;
+  border: 1px solid var(--upnext-ash);
+  border-radius: 6px;
+  padding: 0 !important;
+}
+
+.event-image-container {
+  background-color: rgb(199, 195, 195);
+  height: 100px;
+  padding: 0 !important;
+}
+</style>
