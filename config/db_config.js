aaca2f7c3189a84ab66855ecaf6e84b968bdc67d
nicholaschun <nicholaschunrayne@gmail.com>
2020-01-31 02:30:08 +0000

    added eb deploy support

--- config/db_config.js
+++ config/db_config.js
@@ -14,10 +14,11 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'upnext_db',
-    host: '127.0.0.1',
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
     dialect: 'mysql'
   }
 }
